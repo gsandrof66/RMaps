d90f3550f91ab4a8f59422f22618bc8464d3f065
gsandrof66 <gsandrof66@gmail.com>
2024-10-04 17:29:49 +0100

    first commit

---
+++ map.R
@@ -0,0 +1,70 @@
+# install.packages('sf')
+# install.packages('leaflet')
+# install.packages("leaflet.extras")
+library(leaflet)
+# library(leaflet.extras)
+library(sf)
+library(data.table)
+library(dplyr)
+library(glue)
+
+file_location <- "https://martinjc.github.io/UK-GeoJSON/json/sco/topo_lad.json"
+gedf <- st_read(file_location)
+
+# leaflet() |> 
+#   addTiles(urlTemplate = "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+#            attribution = '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>') |> 
+#   setView(lng = -0.1276, lat = 51.5074, zoom = 10)
+
+df <- data.table("LAD13NM" = c("Glasgow City", "City of Edinburgh", "Fife", "North Lanarkshire", "South Lanarkshire", "Aberdeenshire", "Highland", "Aberdeen City", "West Lothian", "Renfrewshire", "Falkirk", "Perth and Kinross", "Dumfries and Galloway", "Dundee City", "North Ayrshire", "East Ayrshire", "Angus", "Scottish Borders", "South Ayrshire", "East Lothian", "East Dunbartonshire", "East Renfrewshire", "Moray", "Midlothian", "Stirling", "West Dunbartonshire", "Argyll and Bute", "Inverclyde", "Clackmannanshire", "Na h-Eileanan Siar", "Shetland Islands", "Orkney Islands"),
+                 "population" = c(635130, 526470, 374730, 341400, 322630, 262690, 238060, 227430, 185580, 179940, 160700, 153810, 148790, 148060, 135280, 121840, 116260, 115270, 112610, 106370, 106370, 95170, 95510, 92150, 94330, 89590, 86890, 78150, 51400, 26830, 23080, 22500))
+
+setdiff(sort(unique(df$LAD13NM)),
+        sort(unique(gedf$LAD13NM)))
+
+setdiff(sort(unique(gedf$LAD13NM)),
+        sort(unique(df$LAD13NM)))
+
+df |> filter(LAD13NM == "Na h-Eileanan Siar")
+
+df <- df |> mutate(LAD13NM = ifelse(LAD13NM == "Na h-Eileanan Siar", 
+                                    "Eilean Siar", LAD13NM))
+
+# join data sets
+mergescot <- inner_join(gedf, df, by=c("LAD13NM")) |> 
+  dplyr::select(LAD13NM, population, geometry) |> 
+  arrange(desc(population))
+
+leaflet(mergescot) |> 
+  # addTiles() |> 
+  #addTiles(urlTemplate = "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+  #         attribution = '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>') |> 
+  addProviderTiles(provider = providers$CartoDB.DarkMatter) |> 
+  addPolygons(
+    fillColor = ~colorQuantile("YlOrRd", population)(population),
+    weight = 2,
+    opacity = 1,
+    color = "white",
+    dashArray = "3",
+    fillOpacity = 0.7,
+    highlightOptions = highlightOptions(
+      weight = 5,
+      color = "yellow",
+      dashArray = "",
+      fillOpacity = 0.7,
+      bringToFront = TRUE
+    ),
+    label = ~glue("{LAD13NM} {population}"),
+    labelOptions = labelOptions(
+      # style = list("font-weight" = "normal", padding = "3px 8px"),
+      textsize = "15px",
+      direction = "auto"
+    )
+  ) |> 
+  addLegend(
+    pal = colorQuantile("YlOrRd", NULL),
+    values = ~population,
+    opacity = 0.7,
+    title = "Population",
+    position = "bottomright"
+  )
