c24e3b839472a80de8461822b303a22de4d113f2
Gatz Strongs <gsandrof66@gmail.com>
2024-10-09 10:16:39 +0100

    us map

--- map.R
+++ map.R
@@ -11,6 +11,7 @@ library(glue)
 file_location <- "https://martinjc.github.io/UK-GeoJSON/json/sco/topo_lad.json"
 gedf <- st_read(file_location)
 
+
 # leaflet() |> 
 #   addTiles(urlTemplate = "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
 #            attribution = '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>') |> 
@@ -68,3 +69,44 @@ leaflet(mergescot) |>
     title = "Population",
     position = "bottomright"
   )
+
+# US parks
+most_visited_nps_species_data <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-08/most_visited_nps_species_data.csv' |> 
+  fread()
+
+setdiff(unique(most_visited_nps_species_data$ParkCode),
+        unique(data$UNIT_CODE))
+
+data <- "https://gist.githubusercontent.com/erincaughey/2f221501645757e28b715c4063e87595/raw/a90be1b434b1a8cdf71c2abc3373ca63987e2d23/nps-geo-boundary.json" |> 
+  st_read(as_tibble = T) |> dplyr::select(UNIT_CODE, UNIT_NAME, geometry)
+
+us <- 'https://gist.githubusercontent.com/erincaughey/4ea1aaceeb4989bc4700bd248bb664a7/raw/8ebb3b3953c141b560b704d5054b2fa122eaadd5/us-census17-geo.json' |> 
+  st_read(as_tibble = T) |> dplyr::select(NAME, geometry)
+
+leaflet(data) |> 
+  addProviderTiles(provider = providers$CartoDB.DarkMatter) |> 
+  addPolygons(
+    # fillColor = ~colorQuantile("YlOrRd", population)(population),
+    weight = 2,
+    opacity = 1,
+    color = "pink",
+    dashArray = "3",
+    fillOpacity = 0.9,
+    highlightOptions = highlightOptions(
+      weight = 5,
+      color = "yellow",
+      dashArray = "",
+      fillOpacity = 0.7,
+      bringToFront = TRUE
+    ),
+    label = ~glue("{UNIT_NAME}"),
+    labelOptions = labelOptions(
+      # style = list("font-weight" = "normal", padding = "3px 8px"),
+      textsize = "15px",
+      direction = "auto"
+    )
+  )
+  
+
+
+
